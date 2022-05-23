54712413952e2e9a0314754277c6f6c15953c8ee
NastassiaLoginava <nastik.95@bk.ru>
2022-05-23 23:13:08 +0300

    feat: add changes in the solution of the task 03-files-in-folder (Mon, May 23, 2022 11:13:12 PM)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,31 +6,29 @@ const path = require('path');
 // example - txt - 128.369kb
 // path.extname()
 
-fs.readdir(path.join(__dirname, './secret-folder'), { withFileTypes: true }, (error, files) => {       
-    
-        if (error) 
-     console.log(error);
-    else {
-        const filesArray = files.filter(fileName => fileName.isFile()).map(fileName => fileName.name);
+fs.readdir(path.join(__dirname, './secret-folder'), { withFileTypes: true }, (error, files) => {
+  if (error) console.log(error);
+  else {
+    const filesArray = files.filter((fileName) => fileName.isFile()).map((fileName) => fileName.name);
 
-        const filesExt = filesArray.map(el => path.extname(el)).map(el => el.slice(1));
-        
-        const fileFirstPart = filesArray.map(d => path.parse(d).name);
+    const filesExt = filesArray.map((el) => path.extname(el)).map((el) => el.slice(1));
 
-        const filesLink = filesArray.map(e => './secret-folder/' + e);
-        
-        const filesSize = filesLink.map((a) => {
-            try {
-              return fs.statSync(path.resolve(__dirname, a)).size;
-            } catch (error) {
-              console.log(error);
-            }
-          });
-    
+    const fileFirstPart = filesArray.map((d) => path.parse(d).name);
 
-    console.log(fileFirstPart[0] + ' - ' + filesExt[0] + ' - ' + filesSize[0] + 'b')
-    console.log(fileFirstPart[1] + ' - ' + filesExt[1] + ' - ' + filesSize[1] + 'b')
-    console.log(fileFirstPart[2] + ' - ' + filesExt[2] + ' - ' + filesSize[2] + 'b')
-    console.log(fileFirstPart[3] + ' - ' + filesExt[3] + ' - ' + filesSize[3] + 'b')
-    }  
-})
+    const filesLink = filesArray.map((e) => './secret-folder/' + e);
+
+    filesLink.forEach((a, index) => {
+      fs.stat(path.resolve(__dirname, a), (error, stats) => {
+        if (error) {
+          console.log(error);
+        } else {
+          formateResponse(filesExt[index], fileFirstPart[index], stats.size);
+        }
+      });
+    });
+  }
+});
+
+const formateResponse = (filesExt, fileFirstPart, filesSize) => {
+  console.log(fileFirstPart + ' - ' + filesExt + ' - ' + filesSize + 'b');
+};
