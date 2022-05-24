702fa13ca2876e39df6efffe862de22538a81354
NastassiaLoginava <nastik.95@bk.ru>
2022-05-24 21:10:04 +0300

    feat: add changes in the solution of the task 01-read-file (Tue, May 24, 2022 09:08:22 PM)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,21 @@
+// const fs = require('fs');
+// const path = require('path');
+
+// fs.readFile(
+//     path.join(__dirname, './text.txt'),
+//     'utf-8',
+//     (err, data) => {
+//         if (err) throw err;
+//         console.log(data);
+//     }
+// );
+
+
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(
-    path.join(__dirname, './text.txt'),
-    'utf-8',
-    (err, data) => {
-        if (err) throw err;
-        console.log(data);
-    }
-);
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+let fullContent = '';
+readStream.on('data', chunk => fullContent += chunk);
+readStream.on('end', () => process.stdout.write(fullContent));
